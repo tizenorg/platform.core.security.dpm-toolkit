ea3b068f9bb49d3a67325b8c7be03e2cdf39ef4b
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-05-04 16:43:41 +0900

    Add tests for new Wifi policy APIs
    Change-Id: I9f1c1f63f669785f20278dbda995dd04bc25b7ee
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- src/wifi.c
+++ src/wifi.c
@@ -1,36 +1,251 @@
+#include <dpm/wifi.h>
+
 #include "dpm-toolkit.h"
 
-int set_allow_wifi_handler(struct dpm_toolkit_entity* self)
+static char radio_text[][MAX_RADIO_TEXT_LEN] = {"Disallow", "Allow"};
+
+int wifi_set_profile_change_restriction_handler(struct dpm_toolkit_entity* self)
 {
-	dlog_print(DLOG_DEBUG, LOG_TAG, "set_allow_wifi_handler");
-	return POLICY_RESULT_SUCCESS;
+	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	handler_display_radio_popup("Restriction", self, radio_text, sizeof(radio_text) / sizeof(char) / MAX_RADIO_TEXT_LEN);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_set_profile_change_restriction(policy, self->radio_index);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	return (ret == DPM_ERROR_NONE) ? POLICY_RESULT_SUCCESS : POLICY_RESULT_FAIL;
 }
 
-int get_allow_wifi_handler(struct dpm_toolkit_entity* self)
-{
-	dlog_print(DLOG_DEBUG, LOG_TAG, "get_allow_wifi_handler");
-	return POLICY_RESULT_SUCCESS;
+int wifi_is_profile_change_restricted_handler(struct dpm_toolkit_entity* self)
+{	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int state, ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_is_profile_change_restricted(policy, &state);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	if (ret == DPM_ERROR_NONE) {
+        	display_result_popup("Restriction state", state? "Disallowed":"Allowed");
+
+		return POLICY_RESULT_NONE;
+	}
+
+	return POLICY_RESULT_FAIL;
 }
 
-int get_ssid_handler(struct dpm_toolkit_entity* self)
+int wifi_set_network_access_restriction_handler(struct dpm_toolkit_entity* self)
 {
-	dlog_print(DLOG_DEBUG, LOG_TAG, "get_ssid_handler");
-	return POLICY_RESULT_SUCCESS;
+	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	handler_display_radio_popup("Restriction", self, radio_text, sizeof(radio_text) / sizeof(char) / MAX_RADIO_TEXT_LEN);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_set_network_access_restriction(policy, self->radio_index);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	return (ret == DPM_ERROR_NONE) ? POLICY_RESULT_SUCCESS : POLICY_RESULT_FAIL;
+}
+
+int wifi_is_network_access_restricted_handler(struct dpm_toolkit_entity* self)
+{
+	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int state, ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_is_network_access_restricted(policy, &state);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	if (ret == DPM_ERROR_NONE) {
+        	display_result_popup("Restriction state", state? "Disallowed":"Allowed");
+
+		return POLICY_RESULT_NONE;
+	}
+
+	return POLICY_RESULT_FAIL;
+}
+
+int wifi_add_ssid_to_blocklist_handler(struct dpm_toolkit_entity* self)
+{
+	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	handler_display_input_popup("SSID", self);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_add_ssid_to_blocklist(policy, self->entry_input);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	return (ret == DPM_ERROR_NONE) ? POLICY_RESULT_SUCCESS : POLICY_RESULT_FAIL;
+}
+
+int wifi_remove_ssid_from_blocklist_handler(struct dpm_toolkit_entity* self)
+{
+	dpm_wifi_policy_h policy;
+	dpm_context_h context;
+        char *zone;
+	int ret;
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, __func__);
+
+	handler_display_input_popup("SSID", self);
+
+	context = dpm_context_create();
+	if (context == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to create client context");
+		return POLICY_RESULT_FAIL;
+	}
+
+        zone = get_now_zone();
+	policy = dpm_context_acquire_wifi_policy(context, zone);
+        free(zone);
+
+	if (policy == NULL) {
+		dlog_print(DLOG_DEBUG, LOG_TAG, "Failed to get wifi policy interface");
+		dpm_context_destroy(context);
+		return POLICY_RESULT_FAIL;
+	}
+
+	ret = dpm_wifi_remove_ssid_from_blocklist(policy, self->entry_input);
+
+	dpm_context_release_wifi_policy(context, policy);
+	dpm_context_destroy(context);
+
+	return (ret == DPM_ERROR_NONE) ? POLICY_RESULT_SUCCESS : POLICY_RESULT_FAIL;
 }
 
 dpm_toolkit_entity_t dpm_toolkit_wifi_policy[] = {
 	{
-	 .id = "SET_ALLOW_WIFI",
-	 .handler = set_allow_wifi_handler
+	 .id = "WIFI_SET_PROFILE_CHANGE_RESTRICTION",
+	 .handler = wifi_set_profile_change_restriction_handler
 	},
 	{
-	 .id = "GET_ALLOW_WIFI",
-	 .handler = get_allow_wifi_handler
+	 .id = "WIFI_IS_PROFILE_CHANGE_RESTRICTED",
+	 .handler = wifi_is_profile_change_restricted_handler
 	},
 	{
-	 .id = "GET_SSID",
-	 .handler = get_ssid_handler
-	}
+	 .id = "WIFI_SET_NETWORK_ACCESS_RESTRICTION",
+	 .handler = wifi_set_network_access_restriction_handler
+	},
+	{
+	 .id = "WIFI_IS_NETWORK_ACCESS_RESTRICTED",
+	 .handler = wifi_is_network_access_restricted_handler
+	},
+	{
+	 .id = "WIFI_ADD_SSID_TO_BLOCKLIST",
+	 .handler = wifi_add_ssid_to_blocklist_handler
+	},
+	{
+	 .id = "WIFI_REMOVE_SSID_FROM_BLOCKLIST",
+	 .handler = wifi_remove_ssid_from_blocklist_handler
+	},
 };
 
 dpm_toolkit_policy_group_t wifi_policy_group = {
