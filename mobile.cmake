b8aad69c66ff06c2d18e587de9b577ce27e749be
Jaemin Ryu <jm77.ryu@samsung.com>
2016-05-23 09:16:20 +0900

    Support build featuring
    Change-Id: Ic0130028466949d999efff0262dfcbbb7555c1da
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ mobile.cmake
@@ -0,0 +1,9 @@
+SET(TESTCASES
+	src/password.c
+	src/security.c
+	src/wifi.c
+	src/restriction.c
+	src/zone.c
+	src/bluetooth.c
+)
+
---
+++ wearable.cmake
@@ -0,0 +1,8 @@
+SET(TESTCASES
+	src/password.c
+	src/security.c
+	src/wifi.c
+	src/restriction.c
+	src/bluetooth.c
+)
+
