870b5136b3de0a9035a9ddea83bfbb0d0605b321
Jaemin Ryu <jm77.ryu@samsung.com>
2016-05-31 20:46:03 +0900

    Add application policy test interface
    Change-Id: I22aa394d07cbf2108072f4692fdf2b8f9c450d65
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

--- mobile.cmake
+++ mobile.cmake
@@ -15,6 +15,7 @@
 #
 
 SET(TESTCASES
+	src/application.c
 	src/password.c
 	src/security.c
 	src/wifi.c
--- wearable.cmake
+++ wearable.cmake
@@ -1,8 +1,8 @@
 SET(TESTCASES
+	src/application.c
 	src/password.c
 	src/security.c
 	src/wifi.c
 	src/restriction.c
 	src/bluetooth.c
 )
-
