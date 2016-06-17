d373073f435cafae9bd838e4b3711f760238314a
Jaemin Ryu <jm77.ryu@samsung.com>
2016-06-17 16:57:14 +0900

    Remove unsupported policy testcases
    Change-Id: I0546b3e3338f483990f206b7152b64aad17ee0c2
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

--- common.cmake
+++ common.cmake
@@ -18,7 +18,5 @@ SET(TESTCASES
 	src/application.c
 	src/password.c
 	src/security.c
-	src/wifi.c
 	src/restriction.c
-	src/bluetooth.c
 )
--- tv.cmake
+++ tv.cmake
@@ -1,7 +1,5 @@
 SET(TESTCASES
 	src/application.c
 	src/security.c
-	src/wifi.c
 	src/restriction.c
-	src/bluetooth.c
 )
